4b578e7b9ee72aed3e47199c257400f7dcd2e299
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-03-09 21:45:02 +0000

    - Bump version in preparation for release

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'users';
-$app['version'] = '1.0.4';
+$app['version'] = '1.0.6';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-users.spec
+++ packaging/app-users.spec
@@ -1,7 +1,7 @@
 
 Name: app-users
 Epoch: 1
-Version: 1.0.4
+Version: 1.0.6
 Release: 1%{dist}
 Summary: Users
 License: GPLv3
