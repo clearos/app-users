d332cfd4c7841303fcb61bf3af6c24edf51c9f3c
pcbaldwin <pbaldwin@clearfoundation.com>
2015-02-05 21:06:53 -0500

    Prepping for snapshot build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'users';
-$app['version'] = '2.0.5';
+$app['version'] = '2.0.18';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-users.spec
+++ packaging/app-users.spec
@@ -1,7 +1,7 @@
 
 Name: app-users
 Epoch: 1
-Version: 2.0.5
+Version: 2.0.18
 Release: 1%{dist}
 Summary: Users
 License: GPLv3
